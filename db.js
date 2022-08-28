dc5a333d7ee033d23a81e965481c3df0d9330845
Allen <allenpereira17@gmail.com>
2022-08-27 23:41:48 -0400

    adds electron-forge

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./mydb.db', sqlite3.OPEN_READWRITE, (err) => {
+const db = new sqlite3.Database(__dirname + './mydb.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err)
         return console.error(err.message);
     
